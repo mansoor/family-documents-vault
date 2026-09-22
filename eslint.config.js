5989adf2798471900d7be55ab6d04957582ab177
Mansoor <mansoor.seikh@gmail.com>
2026-09-21 23:35:45 -0700

    style: format eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,13 @@ import prettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   {
-    ignores: ['**/node_modules/**', '**/dist/**', '**/coverage/**', 'docs-private/**', 'prototype/**'],
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/coverage/**',
+      'docs-private/**',
+      'prototype/**',
+    ],
   },
   js.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
